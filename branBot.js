2f87ed515a0f2e62243d415cb381d549415b04ae
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2021-02-02 12:08:26 -0500

    Vc Time Will update when +stats is called to give most updated stats

--- branBot.js
+++ branBot.js
@@ -2,17 +2,21 @@ var mysql = require('mysql');
 var connection = require('./connect').establishConnect();
 var token = require('./token');
 
-connection.query('SELECT * FROM stats', function (error, results, fields) {
+/*connection.query('SELECT * FROM stats', function (error, results, fields) {
     if (error)
         throw error;
 
     results.forEach(result => {
         var xp = Math.floor(result.vcTime/30);
-        for (var i = 0; i < result) {
-
+        for (var i = 0; i < result.messageCount; i++) {
+          xp += (Math.floor(Math.random() * 3) + 2);
         }
+        var sql = "UPDATE stats SET xp = ? WHERE id= ?";
+        connection.query(sql, [xp, result.id], function (err, result) {
+          if (err) throw err;
+        });
     });
-});
+});*/
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -61,17 +65,17 @@ function identify(inp, msg) {
     return msg.guild.member(msg.mentions.users.first());
   } else {
     var member;
-    member = msg.guild.members.cache.array().filter((member) => {
+    member = msg.guild.members.cache.array().find((member) => {
       return member.displayName.toLowerCase() === inp.toLowerCase();
-    })[0];
+    });
     if (member === undefined) {
-      member = msg.guild.members.cache.array().filter((member) => {
+      member = msg.guild.members.cache.array().find((member) => {
         return member.user.id === inp;
-      })[0];
+      });
     }
     return member;
   }
-  return null;
+  return undefined;
 }
 
 function level (m, vc) {
@@ -104,7 +108,9 @@ function DMInterference(newState, oldState) {
 
 
 //Gifs
-eatGifs = ["https://media1.tenor.com/images/48679297034b0f3f6ee28815905efae8/tenor.gif", "https://media1.tenor.com/images/c0c0f8bb63f38f0ddf6a736354987050/tenor.gif", "https://media1.tenor.com/images/c10b4e9e6b6d2835b19f42cbdd276774/tenor.gif", "https://media1.tenor.com/images/3e4d211cd661a2d7125a6fa12d6cecc6/tenor.gif", "https://media1.tenor.com/images/0de27657daa673ccd7a60cf6919084d9/tenor.gif", "https://media2.giphy.com/media/iWkHDNtcHpB5e/giphy.gif"];
+var eatGifs = ["https://media1.tenor.com/images/48679297034b0f3f6ee28815905efae8/tenor.gif", "https://media1.tenor.com/images/c0c0f8bb63f38f0ddf6a736354987050/tenor.gif", "https://media1.tenor.com/images/c10b4e9e6b6d2835b19f42cbdd276774/tenor.gif", "https://media1.tenor.com/images/3e4d211cd661a2d7125a6fa12d6cecc6/tenor.gif", "https://media1.tenor.com/images/0de27657daa673ccd7a60cf6919084d9/tenor.gif", "https://media2.giphy.com/media/iWkHDNtcHpB5e/giphy.gif"];
+
+var scaredGifs = ["https://media1.tenor.com/images/9377aa4eda2e4ff3789ff40000afcc8e/tenor.gif", "https://media1.tenor.com/images/43270d3659218523b11fa4cc8bbb370f/tenor.gif"];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -161,6 +167,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 });
 
 client.on('message', msg => {
+
   if (msg.content === '+ping') {
     msg.reply("pong");
   }
@@ -223,11 +230,21 @@ client.on('message', msg => {
     msg.channel.send(embed);
   }
 
+  if (msg.content === '+scared') {
+    var embed = new Discord.MessageEmbed();
+    embed.setDescription("**" + msg.member.displayName + "** is scared");
+    embed.setColor("#00bfff");
+    var gif = randomInd(scaredGifs);
+    embed.setImage(gif);
+    msg.channel.send(embed);
+  }
+
+
 
   //member info
   if (msg.content.includes('+member')) {
-    var parser = msg.content.split(" ");
-    if (parser.length == 1) {
+    var parser = msg.content.split(" "); //+member Tuanson returns ["+member", "Tuanson"]
+    if (parser.length == 1) {// +member
       //Defaults to identifying the author of the message
       var roles = msg.member.roles.cache.array().join(", ");
       var joinedAt = msg.member.joinedAt.toString().split(" ");
@@ -249,7 +266,7 @@ client.on('message', msg => {
     } else {
       //If identifying a specific member
       var info = identify(parser[1], msg);
-      if (info === null || info.user.bot === true) {
+      if (info === undefined || info.user.bot === true) {
         msg.channel.send("Unable to Identify the User Specified");
       } else {
         var member = info;
@@ -330,34 +347,63 @@ client.on('message', msg => {
       member = identify(parser[1], msg);
     }
     if (member !== null && member.user.bot === false) {
+      var check = "SELECT joinedAt FROM vctracking WHERE userID= ? AND guildID= ?";
+      connection.query(check, [member.id, member.guild.id], function (err, time, fields) {
+        if (time.length !== 0) {
+          connection.query("DELETE FROM vctracking WHERE userID= ? AND guildID = ?", [member.id, member.guild.id], function (err, ranking) {
+            var check = "SELECT * FROM stats WHERE userID= ? AND guildID= ?";
+            connection.query(check, [member.id, member.guild.id], function (err, result, fields) {
+              var present = new Date();
+              var difference = Math.floor(present/1000) - time[0].joinedAt;
+              if (result.length === 0) {
+                var sql = "INSERT INTO stats (userID, guildID, vcTime, xp) VALUES ?";
+                var values = [[member.id, member.guild.id, difference, Math.floor(difference/30)]];
+                connection.query(sql, [values], function (err, result) {
+                  if (err) throw err;
+                });
+              } else {
+                var sql = "UPDATE stats SET vcTime = ?, xp = xp + ? WHERE userID= ? AND guildID= ?";
+                connection.query(sql, [result[0].vcTime+difference, Math.floor(difference/30), member.id, member.guild.id], function (err, result) {
+                  if (err) throw err;
+                });
+              }
+            });
+          });
+        }
+      });
+
       connection.query("SELECT * FROM stats WHERE userID = ? AND guildID = ?", [member.user.id, msg.guild.id], function (err, result) {
         if (err) throw err;
-        var messageCount = result[0].messageCount;
-        if (messageCount === null) {
-          messageCount = 0;
+        if (result.length === 0) {
+          msg.reply("No stats exist for specified User");
+        } else {
+          var messageCount = result[0].messageCount;
+          if (messageCount === null) {
+            messageCount = 0;
+          }
+          var vcTime = result[0].vcTime;
+          if (vcTime === null) {
+            vcTime = 0;
+          }
+          var xp = result[0].xp;
+          if (xp > 1000) {
+            xp = Math.floor(xp/100)/10 + "k";
+          }
+          connection.query("SELECT userID FROM stats WHERE guildID = ? ORDER BY xp DESC", [msg.guild.id], function (err, ranking) {
+            var rank = ranking.indexOf(ranking.find((id) => id.userID === member.user.id)) + 1;
+            var embed = new Discord.MessageEmbed();
+            embed.setTitle(member.user.tag);
+            embed.setColor("#FFD700");
+            embed.setThumbnail(member.user.avatarURL());
+            embed.addFields(
+              { name: "RANK", value: "#" + rank},
+              { name: "Messages Sent", value: messageCount, inline: true},
+              { name: "Time in Call", value: timeStamp(vcTime), inline: true},
+              { name: "XP", value: xp }
+            );
+            msg.channel.send(embed);
+          });
         }
-        var vcTime = result[0].vcTime;
-        if (vcTime === null) {
-          vcTime = 0;
-        }
-        var xp = result[0].xp;
-        if (xp > 1000) {
-          xp = Math.floor(xp/100)/10 + "k";
-        }
-        connection.query("SELECT userID FROM stats WHERE guildID = ? ORDER BY xp DESC", [msg.guild.id], function (err, ranking) {
-          var rank = ranking.indexOf(ranking.find((id) => id.userID === member.user.id)) + 1;
-          var embed = new Discord.MessageEmbed();
-          embed.setTitle(member.user.tag);
-          embed.setColor("#FFD700");
-          embed.setThumbnail(member.user.avatarURL());
-          embed.addFields(
-            { name: "RANK", value: "#" + rank},
-            { name: "Messages Sent", value: messageCount, inline: true},
-            { name: "Time in Call", value: timeStamp(vcTime), inline: true},
-            { name: "XP", value: xp }
-          );
-          msg.channel.send(embed);
-        });
       });
     } else {
       msg.channel.send("Unable to Identify the User Specified");
